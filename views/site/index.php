87967ef008b93ce40e22bc27b2515663a34ce9cb
AnSima <bessima13@yandex.ru>
2018-05-02 13:53:00 +0300

    Убран функционал для работы с авторизацией пользователей

--- views/site/index.php
+++ views/site/index.php
@@ -7,43 +7,38 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Добро пожаловать!</h1>
+        <h1>Рекрутинг</h1>
 
-        <p class="lead">Данный сервис предназначен для принятия решения о найме сотрудников в вашу фирму</p>
+        <p class="lead">Данный сервис предназначен для принятия решения о найме сотрудников</p>
+        <p class="lead">Выберите дальнейшее действие: </p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Добавление профессии</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <a
+                    class="btn btn-info btn-lg center-block"
+                    role="button"
+                    href="/candidate">
+                    Список менеджеров
+                </a>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <a
+                    class="btn btn-info btn-lg center-block"
+                    role="button"
+                    href="/study">
+                    Обучение
+                </a>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <a
+                    class="btn btn-info btn-lg center-block"
+                    role="button"
+                    href="/testing">
+                    Тестирование
+                </a>
             </div>
         </div>
 
